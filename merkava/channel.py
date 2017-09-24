8a97cb648189e56ecb963d0f29fed034c9bbbf7c
Adam Hopkins <admhpkns@gmail.com>
2017-09-24 22:48:32 +0300

    Rework classes to add abstractions

--- merkava/channel.py
+++ merkava/channel.py
@@ -2,14 +2,12 @@ import os
 import msgpack
 from datetime import datetime
 import pytz
+# import asyncio
 
 
 class Result(object):
     def __init__(self, data):
         self.data = data
-
-    def __iter__(self):
-        return {key: value for key, value in self.data}
 
     @property
     def is_deleted(self):
@@ -20,75 +18,80 @@ class Channel(object):
     def __init__(self, channel_name, data_path):
         self.channel_name = channel_name.lower()
         self.data_path = data_path
-        self.index_path = os.path.join(data_path, '{}.mrkv'.format(self.channel_name))
+        self.index_path = os.path.join(
+            data_path,
+            '{}.mrkv'.format(self.channel_name)
+        )
         if not os.path.exists(self.index_path):
             self.index = 0
             self._store_index()
         else:
-            with open(self.index_path, 'rb') as store:
-                self.index = msgpack.load(store, use_list=False)
+            self.index = self._load(self.index_path)
+
+    # async def listen(self):
+    #     with open(self.index_path, 'rb') as file:
+    #         file.seek(0, 0)
+
+    #         while True:
+    #             line = file.readline()
+    #             if not line:
+    #                 await asyncio.sleep(0.1)
+    #                 continue
+    #             print(line)
+    #             yield line
 
     def create(self, raw):
-        file_name = '{}.{}.mrkv'.format(self.channel_name, self.index)
-        file_path = os.path.join(self.data_path, file_name)
+        file_path = self._build_path(self.index)
         index = self.index
         data = {
             'id': index,
             'created': datetime.now(tz=pytz.utc).isoformat(),
             'data': raw
         }
-        with open(file_path, 'wb') as store:
-            msgpack.dump(data, store)
+        self._dump(file_path, data)
         self += 1
-        return index, data
+        return 201, data
 
     def retrieve(self, id):
-        file_name = '{}.{}.mrkv'.format(self.channel_name, id)
-        file_path = os.path.join(self.data_path, file_name)
+        file_path = self._build_path(id)
         if not os.path.exists(file_path):
-            return None
+            return 404, None
         with open(file_path, 'rb') as store:
             data = msgpack.load(store)
-        return data
+        return 200, data
 
     def update(self, id, raw):
-        file_name = '{}.{}.mrkv'.format(self.channel_name, id)
-        file_path = os.path.join(self.data_path, file_name)
+        file_path = self._build_path(id)
         data = self.retrieve(id)
         inner_data = data.get(b'data')
         if isinstance(inner_data, dict):
             data.get(b'data').update(raw)
         else:
             data[b'data'] = raw
-        with open(file_path, 'wb') as store:
-            msgpack.dump(data, store)
-        return data
+        self._dump(file_path, data)
+        return 200, data
 
     def delete(self, id):
-        file_name = '{}.{}.mrkv'.format(self.channel_name, id)
-        file_path = os.path.join(self.data_path, file_name)
+        file_path = self._build_path(id)
         data = self.retrieve(id)
         data.update({'is_deleted': True})
-        with open(file_path, 'wb') as store:
-            msgpack.dump(data, store)
-        return {'ok': True}
+        self._dump(file_path, data)
+        return 204, {'ok': True}
 
     def restore(self, id):
-        file_name = '{}.{}.mrkv'.format(self.channel_name, id)
-        file_path = os.path.join(self.data_path, file_name)
+        file_path = self._build_path(id)
         data = self.retrieve(id)
         data.update({
             'is_deleted': False,
             'was_restored': True,
         })
-        with open(file_path, 'wb') as store:
-            msgpack.dump(data, store)
-        return data
+        self._dump(file_path, data)
+        return 200, data
 
-    def recent(self):
+    def recent(self, num):
         results = []
         index = self.index - 1
-        while len(results) < 5 and index > 0:
+        while len(results) < num and index > 0:
             result = self.get_result(index)
             if result is not None and not result.is_deleted:
                 results.append(result.data)
@@ -106,6 +109,17 @@ class Channel(object):
         self._store_index()
         return self.index
 
+    def _build_path(self, id):
+        file_name = '{}.{}.mrkv'.format(self.channel_name, id)
+        return os.path.join(self.data_path, file_name)
+
+    def _load(self, path):
+        with open(path, 'rb') as store:
+            return msgpack.load(store, use_list=False)
+
+    def _dump(self, path, data):
+        with open(path, 'wb') as store:
+            msgpack.dump(data, store)
+
     def _store_index(self):
-        with open(self.index_path, 'wb') as store:
-            msgpack.dump(self.index, store)
+        self._dump(self.index_path, self.index)
